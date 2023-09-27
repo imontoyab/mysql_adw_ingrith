e093c5d56d957a792217a8271d8cc901703290c4
Ingrith Montoya <ingrithmontoya@hotmail.com>
2023-09-27 04:41:56 +0000

    fecha

---
+++ scripts/fecha.sql
@@ -0,0 +1,10 @@
+SELECT det.SalesOrderID id_dtll_venta, 
+sh.OrderDate,
+year(sh.OrderDate) as anno_orden,
+month(sh.OrderDate) as mes_orden,
+dayofweek(sh.OrderDate) as dia_orden,
+hour(sh.OrderDate)as hora_orden,
+minute(sh.OrderDate) as minutos_orden
+FROM  Sales_SalesOrderDetail as det
+JOIN Sales_SalesOrderHeader AS sh on sh.SalesOrderID = det.SalesOrderID
+limit 5; 
