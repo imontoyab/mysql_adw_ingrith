fca254936256c2934862fa2aaae02ecc3555c75e
Ingrith Montoya <ingrithmontoya@hotmail.com>
2023-10-10 02:47:42 +0000

    ACTUALIZACION CONSULTAS

--- scripts/prueba.sql
+++ scripts/prueba.sql
@@ -77,3 +77,24 @@ limit 5
 ;
 
 
+
+/*------------TOP 5 MAYOR VALOR VENDIDO POR COMPRADOR -------------*/
+
+WITH comprador AS(
+select dc.nombre_cliente,  (od.cantidad * od.precio_unit) valor
+from orden_detalle od
+join dim_producto as dp on dp.producto_key = od.producto_key
+JOIN dim_territorio AS dt ON dt.territory_key = od.territory_key
+JOIN dim_vendedor AS dv ON dv.vendedor_key = od.vendedor_key
+JOIN dim_fecha as df on df.fecha_key = od.fecha_key
+join dim_cliente as dc on dc.cliente_key  = od.cliente_key 
+), comp_fecha as(
+
+SELECT vd.nombre_cliente, sum(vd.valor) sum_ventas
+FROM comprador vd 
+group by vd.nombre_cliente
+order by sum_ventas desc
+)
+SELECT * FROM comp_fecha
+limit 5
+;
